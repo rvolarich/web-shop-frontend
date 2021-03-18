d0cfd79d51d1105b0cca59739325c37c353608b5
Your Namervolarich <robertvolaric973@gmail.com>
2021-03-18 19:28:54 +0100

    deploy 1

--- src/components/GoodBye.js
+++ src/components/GoodBye.js
@@ -5,9 +5,10 @@ class Goodbye extends React.Component{
 
 render(){
     return(
-        <Container>
-        <h4 style={{marginTop:'5%'}}>Thank you for using services from OneStop-ShipShop!</h4>
-        </Container>
+        
+        <div style={{margin:'auto', minHeight:'410px', paddingTop:'90px', textAlign:'center'}}>
+        <h4 style={{marginTop:'100px'}}>Thank you for using the services of OneStop-ShipShop!</h4>
+        </div>
     )
 }
     
